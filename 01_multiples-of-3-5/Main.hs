3ba0ab5fa759c54304beb07117582a4b1d72a279
preslavmihaylov <preslav@pmihaylov.com>
2025-02-20 11:09:59 +0200

    rename func

--- 01_multiples-of-3-5/Main.hs
+++ 01_multiples-of-3-5/Main.hs
@@ -1,3 +1,3 @@
-main = putStrLn $ show $ multiples 1000 [3, 5]
+main = putStrLn $ show $ sumMultiples 1000 [3, 5]
 
-multiples limit factors = sum [x | x <- [1 .. (limit - 1)], any (\f -> x `mod` f == 0) factors]
+sumMultiples limit factors = sum [x | x <- [1 .. (limit - 1)], any (\f -> x `mod` f == 0) factors]

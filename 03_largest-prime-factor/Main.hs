a77f0019326c166c23b5b197e85de76ab4e6ec8d
preslavmihaylov <preslav@pmihaylov.com>
2025-02-20 15:27:31 +0200

    add alternative solution

--- 03_largest-prime-factor/Main.hs
+++ 03_largest-prime-factor/Main.hs
@@ -3,9 +3,19 @@ import Data.List
 main = putStrLn $ show $ largestPrimeFactor 600851475143
 
 largestPrimeFactor :: Int -> Int
-largestPrimeFactor n = last $ sort $ [x | x <- [1 .. sqrtn], n `mod` x == 0, isPrime x]
-  where
-    sqrtn = round $ sqrt $ fromIntegral n
+largestPrimeFactor n = maxFactor n 2
 
-isPrime :: Int -> Bool
-isPrime n = [x | x <- [1 .. n], n `mod` x == 0] == [1, n]
+maxFactor 1 n = n
+maxFactor target n
+  | target `mod` n == 0 = maxFactor (target `div` n) n
+  | otherwise = maxFactor target (n + 1)
+
+-- Alternative solution:
+--
+-- largestPrimeFactor :: Int -> Int
+-- largestPrimeFactor n = last $ sort $ [x | x <- [1 .. sqrtn], n `mod` x == 0, isPrime x]
+--   where
+--     sqrtn = round $ sqrt $ fromIntegral n
+--
+-- isPrime :: Int -> Bool
+-- isPrime n = [x | x <- [1 .. n], n `mod` x == 0] == [1, n]

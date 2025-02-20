bc1a8048a97e9b90e12d09f360b8ccd255f5e157
preslavmihaylov <preslav@pmihaylov.com>
2025-02-20 11:10:34 +0200

    func signature

--- 01_multiples-of-3-5/Main.hs
+++ 01_multiples-of-3-5/Main.hs
@@ -1,3 +1,4 @@
 main = putStrLn $ show $ sumMultiples 1000 [3, 5]
 
+sumMultiples :: Int -> [Int] -> Int
 sumMultiples limit factors = sum [x | x <- [1 .. (limit - 1)], any (\f -> x `mod` f == 0) factors]
